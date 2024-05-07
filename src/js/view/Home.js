7b257390de4ad7bd38d5f5c669ac00d30e337da7
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2024-05-07 07:53:21 -0500

    fix: :bug: Corrección the bug en el componente de la historia

--- src/js/view/Home.js
+++ src/js/view/Home.js
@@ -8,20 +8,20 @@ export async function cardHistories() {
     const cards = await Promise.all(data.map(({ title, date, details, links }) => {
         const card = document.createElement('div')
         const titleCard = document.createElement('h2')
-        const textCard = document.createElement('p')
+        // const textCard = document.createElement('p')
         const dateCard = document.createElement('span')
         const linkArticle = document.createElement('a')
         const container = document.createElement('div')
 
         titleCard.textContent = title
-        textCard.textContent = details
+        // textCard.textContent = details
         dateCard.textContent = date
         linkArticle.innerHTML = `<i class='bx bx-link'></i>`
 
         // attributes
         card.classList.add("card")
         titleCard.classList.add("card__title")
-        textCard.classList.add("card__details")
+        // textCard.classList.add("card__details")
         dateCard.classList.add("card__date")
         card.classList.add("card")
         linkArticle.classList.add("card__article")
@@ -29,7 +29,8 @@ export async function cardHistories() {
 
         // JOIN
         container.append(dateCard, linkArticle)
-        card.append(titleCard, textCard, container)
+        // card.append(titleCard, textCard, container)
+        card.append(titleCard, container)
         return card
     }))
 
